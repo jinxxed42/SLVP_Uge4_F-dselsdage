52a1ab9707bfb2958c3eada24fb4af8bd2ebef2f
Claus Damgaard <jinxxed@outlook.com>
2023-03-22 16:42:32 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLVP_Uge4_Fødselsdage
+{
+    internal class Person
+    {
+        public string Name { get; private init; }
+        public int Age { get; private set; }
+        public string Address { get; private init; }
+
+        public Person(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+
+        public void fødselsdag()
+        {
+            Age++;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using SLVP_Uge4_Fødselsdage;
+
+Console.WriteLine("Indtast navn på personen:");
+string name = Console.ReadLine();
+if (name == "")
+{
+    Console.WriteLine("Du skal indtaste et eller andet. Programmet stoppes.");
+    return;
+}
+Console.WriteLine("Indtast alder på personen:");
+string agestring = Console.ReadLine();
+if (!int.TryParse(agestring, out int age) || age < 0)
+{
+    Console.WriteLine("Kun positive heltal tak. Programmet stoppes.");
+    return;
+}
+Console.WriteLine("Indtast en addresse på personen:");
+string address = Console.ReadLine();
+if (address == "")
+{
+    Console.WriteLine("Du skal indtaste et eller andet. Programmet stoppes.");
+    return;
+}
+Console.WriteLine();
+Person p1 = new Person(name, age, address);
+Console.WriteLine("Personen er: " + p1.Name + " som er " + p1.Age + " år gammel og bor " + p1.Address + ".");
+p1.fødselsdag();
+p1.fødselsdag();
+Console.WriteLine();
+Console.WriteLine("- Efter to fødselsdage -");
+Console.WriteLine();
+Console.WriteLine("Personen er: " + p1.Name + " som er " + p1.Age + " år gammel og bor " + p1.Address + ".");
+
